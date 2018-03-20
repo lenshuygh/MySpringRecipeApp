c6e747781e2ef1f0c4ab3c40901702403d07bd2b
lenshuygh <lens.huygh@gmail.com>
2018-03-20 20:30:50 +0100

    refactoring with project Lombok for Category-entity>@Data Recipe-entity>@Data
    added logging to serviceImpl
    RecipeServiceImpl>@Slf4j + added string message in method

--- src/main/java/com/lens/gurucourse/recipeproject/recipes/services/RecipeServiceImpl.java
+++ src/main/java/com/lens/gurucourse/recipeproject/recipes/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.lens.gurucourse.recipeproject.recipes.services;
 
 import com.lens.gurucourse.recipeproject.recipes.domain.Recipe;
 import com.lens.gurucourse.recipeproject.recipes.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
@@ -17,10 +19,10 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("i'm in th service");
+
         Set<Recipe> recipeSet = new HashSet<>();
-
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
-
         return recipeSet;
     }
 }
