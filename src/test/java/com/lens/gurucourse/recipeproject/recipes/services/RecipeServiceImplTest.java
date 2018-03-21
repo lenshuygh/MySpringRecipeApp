5f60ef0a82fe2440bbcdb5c99ccf2b841e4d7d1b
lenshuygh <lens.huygh@gmail.com>
2018-03-22 00:13:49 +0100

    created test for RecipeServiceImpl with Mockito

---
+++ src/test/java/com/lens/gurucourse/recipeproject/recipes/services/RecipeServiceImplTest.java
@@ -0,0 +1,48 @@
+package com.lens.gurucourse.recipeproject.recipes.services;
+
+import com.lens.gurucourse.recipeproject.recipes.domain.Recipe;
+import com.lens.gurucourse.recipeproject.recipes.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+
+        Set<Recipe> recipeSet = recipeService.getRecipes();
+
+        assertEquals(recipeSet.size(),1);
+
+        // verify that findAll is called exactly once !!
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
