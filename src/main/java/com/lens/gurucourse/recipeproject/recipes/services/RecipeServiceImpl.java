dc8fa6ec538bb2bb9221f9550c9d3404085507a7
lenshuygh <lens.huygh@gmail.com>
2018-03-27 18:15:19 +0200

    added update for recipe added to service + serviceImpl added to controller, changed psoition of ID in url for 'view' + 'update' added to RecipeControllerTest

--- src/main/java/com/lens/gurucourse/recipeproject/recipes/services/RecipeServiceImpl.java
+++ src/main/java/com/lens/gurucourse/recipeproject/recipes/services/RecipeServiceImpl.java
@@ -55,4 +55,10 @@ public class RecipeServiceImpl implements RecipeService {
         log.debug("saved recipeId -> " + savedRecipe.getId());
         return recipeToRecipeCommand.convert(savedRecipe);
     }
+
+    @Override
+    @Transactional
+    public RecipeCommand findCommandById(Long id) {
+        return recipeToRecipeCommand.convert(findById(id));
+    }
 }
