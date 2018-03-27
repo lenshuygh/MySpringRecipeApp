081e1d9d29519fd7eb7dc775e5c86a91adf34933
lenshuygh <lens.huygh@gmail.com>
2018-03-27 20:32:58 +0200

    added 'delete' link to index.html added 'view' btn to show.html added list.html to view ingredients with options to view/update/del added IngredientController added test for ingredientController

---
+++ src/main/java/com/lens/gurucourse/recipeproject/recipes/controllers/IngredientController.java
@@ -0,0 +1,28 @@
+package com.lens.gurucourse.recipeproject.recipes.controllers;
+
+import com.lens.gurucourse.recipeproject.recipes.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IngredientController {
+    RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/{id}/ingredients")
+    public String getIngredients(@PathVariable String id, Model model){
+        log.debug("Getting ingredient list for recipe id: " + id);
+
+        //use command obj to avoid lazy load errors in thymeleaf
+        model.addAttribute("recipe",recipeService.findCommandById(Long.valueOf(id)));
+
+        return "recipe/ingredient/list";
+    }
+}
