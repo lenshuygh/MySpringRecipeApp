442afccbb1ff7c1e71c7960e1bea7c2de7b07c9f
lenshuygh <lens.huygh@gmail.com>
2018-04-02 00:42:15 +0200

    implmented persisting image + test

--- src/main/java/com/lens/gurucourse/recipeproject/recipes/services/ImageServiceImpl.java
+++ src/main/java/com/lens/gurucourse/recipeproject/recipes/services/ImageServiceImpl.java
@@ -1,17 +1,49 @@
 package com.lens.gurucourse.recipeproject.recipes.services;
 
+import com.lens.gurucourse.recipeproject.recipes.domain.Recipe;
+import com.lens.gurucourse.recipeproject.recipes.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
 
+    RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
-    public void saveImageFile(Long aLong, MultipartFile file) {
+    @Transactional
+    public void saveImageFile(Long recipeId, MultipartFile file) {
 
-        log.debug("received a file");
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
 
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+
+
+            //log.debug("received a file");
+        } catch (IOException e){
+            //todo handle this better
+            log.error("Error occurred");
+            e.printStackTrace();
+        }
     }
 }
