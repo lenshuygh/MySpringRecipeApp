09792cf4be78f5e91f0efcb04cf80894f85c8bed
lenshuygh <lens.huygh@gmail.com>
2018-04-05 23:20:21 +0200

    added 'codecov.io' cobertura coverage config in '.circleci/config.yml' & 'pom.xml'
    added sql scripts to create db and users + priveleges

---
+++ src/main/scripts/configure-mysql.sql
@@ -0,0 +1,30 @@
+#databases
+CREATE DATABASE sfg_dev;
+CREATE DATABASE sfg_prod;
+
+#accounts
+CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'guru';
+CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'guru';
+#for docker because docker will not be running on localhost according to sql, so use wildcard '%'
+CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'guru';
+CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'guru';
+
+
+#Database grants
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+#grant priveleges to other users
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'%';
